581f72d28866267dcc6c7541171f6396596ffd95
Zusldev <liborio07zs@gmail.com>
2022-09-14 21:47:11 -0700

    Añadí try-catch para manejar errores

--- helpers/multiply.js
+++ helpers/multiply.js
@@ -1,19 +1,23 @@
 const fs = require("fs");
 const createFile = async (n = 5) => {
-  console.log("        Tabla del: ", n);
-  console.log("==================================");
+  try {
+    console.log("        Tabla del: ", n);
+    console.log("==================================");
 
-  let result = "";
-  //Multiply
-  for (let i = 1; i <= 10; i++) {
-    result += `${n} * ${i} = ${n * i}\n`;
+    let result = "";
+    //Multiply
+    for (let i = 1; i <= 10; i++) {
+      result += `${n} * ${i} = ${n * i}\n`;
+    }
+    //Show result in console
+    console.log(result);
+    //write file
+    fs.writeFileSync(`table${n}.txt`, result);
+
+    return `table${n}.txt`;
+  } catch (error) {
+    throw error;
   }
-  //Show result in console
-  console.log(result);
-  //write file
-  fs.writeFileSync(`table${n}.txt`, result);
-
-  return `table${n}.txt`;
 };
 
 //Export module
