82ddac44c120b3602762fe946e075c4a559ba8bd
Zusldev <liborio07zs@gmail.com>
2022-09-14 21:44:51 -0700

    segunda manera de crear una promesa en multiply

--- helpers/multiply.js
+++ helpers/multiply.js
@@ -1,21 +1,19 @@
 const fs = require("fs");
-const createFile = (n = 5) => {
-  return new Promise((resolve, reject) => {
-    console.log("        Tabla del: ", n);
-    console.log("==================================");
+const createFile = async (n = 5) => {
+  console.log("        Tabla del: ", n);
+  console.log("==================================");
 
-    let result = "";
-    //Multiply
-    for (let i = 1; i <= 10; i++) {
-      result += `${n} * ${i} = ${n * i}\n`;
-    }
-    //Show result in console
-    console.log(result);
-    //write file
-    fs.writeFileSync(`table${n}.txt`, result);
+  let result = "";
+  //Multiply
+  for (let i = 1; i <= 10; i++) {
+    result += `${n} * ${i} = ${n * i}\n`;
+  }
+  //Show result in console
+  console.log(result);
+  //write file
+  fs.writeFileSync(`table${n}.txt`, result);
 
-    resolve(`table${n}.txt`);
-  });
+  return `table${n}.txt`;
 };
 
 //Export module
