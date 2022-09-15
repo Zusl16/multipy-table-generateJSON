622376fb3a90b67e19f0aa80f51325f32be0e0a9
Zusldev <liborio07zs@gmail.com>
2022-09-14 22:09:26 -0700

    Add package.json

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 const { createFile } = require("./helpers/multiply");
 //Limpiar consola
 console.clear();
+
+//Get arguments from console and convert to number
+//NEVER DO THIS, BAD PRACTICE, USE YARGS
+console.log(process.argv);
+const [, , arg3 = "n=5"] = process.argv;
+const [, n = 5] = arg3.split("=");
+
 //CONSTANT NUMBER
-const n = 4;
-//CALL FUNCTION
+// const n = 4;
+
+// //CALL FUNCTION
 createFile(n)
   .then((result) => console.log(result, "created successfully"))
   .catch((err) => console.log(err, "error"));
-
